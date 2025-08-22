1ea5ab9bbd53fa9269abf52c0c9c688422218f70
Arshad2502 <arshad.jhs25@gmail.com>
2025-08-23 00:07:58 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+
+variable "prefix" {
+  description = "The prefix which should be used for all resources"
+  default  = "ars"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources should be created."
+  default = "North Europe"
+}
+
+variable "admin_password" {
+  description = "Admin password for postgresSQL db"
+  type        = string
+  sensitive   = true
+}
+
+variable "admin_username" {
+  description = "Admin username for postgresSQL db"
+  type        = string
+  sensitive   = true
+} 
